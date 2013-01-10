b023c5c2783b88ab5106ac0ec92e7accb275b5a5
Jung Sang-jun <bandoche@hanmail.net>
2013-01-10 21:19:47 +0900

    Initial Commit

---
+++ FaceBotPreference/FaceBotPreference.h
@@ -0,0 +1,15 @@
+//
+//  FaceBotPreference.h
+//  FaceBotPreference
+//
+//  Created by Sang-jun Jung on 13. 1. 10..
+//  Copyright (c) 2013년 Sang-jun Jung. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface FaceBotPreference : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ FaceBotPreference/FaceBotPreference.m
@@ -0,0 +1,17 @@
+//
+//  FaceBotPreference.m
+//  FaceBotPreference
+//
+//  Created by Sang-jun Jung on 13. 1. 10..
+//  Copyright (c) 2013년 Sang-jun Jung. All rights reserved.
+//
+
+#import "FaceBotPreference.h"
+
+@implementation FaceBotPreference
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
